f4940cb7a9c77b9ec74e46495c9b5b2c63b48f4c
25MCMT07_VAGHMARE_SIDDHARTH <25mcmt07@ailab-ol05.dcis>
2026-01-13 15:45:40 +0530

    second commit

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int fibonaci(int n){
+
+    if(n == 0 || n==1){
+        return n;
+    }
+    return fibonaci(n-1)+fibonaci(n-2);
+}
+
+int main(){
+    int n =7;
+    fibonaci(n);
+    return 0;
+}
